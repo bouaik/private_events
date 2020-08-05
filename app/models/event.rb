deeb060ac9fa26210fd36284f1f215c03a720b99
bouaik <bouaik.lhou@outlook.fr>
2020-08-05 22:04:01 +0100

    make association in models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: :User
+    has_many :attandances, through: :event_attendances, source: :attende
+    has_many :event_attendances, foreign_key: :attendances_id, class_name: :EventAttendance
+
 end
