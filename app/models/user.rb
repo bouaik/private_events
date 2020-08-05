0ae9a1afdd733c5617604c3fea3c74cf26051007
bouaik <bouaik.lhou@outlook.fr>
2020-08-05 16:32:33 +0100

    make association bt user and events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-    has_many :events, foreign_key: :creator_id, class_name: :Post
+    has_many :events, foreign_key: :creator_id, class_name: :Event
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_143741) do
+ActiveRecord::Schema.define(version: 2020_08_05_145539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_08_05_143741) do
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
