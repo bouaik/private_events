26e23660d3c20e0e1fc3962ba02cac246ad837af
bouaik <bouaik.lhou@outlook.fr>
2020-08-05 01:30:17 +0100

    make Authentication with session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
-  root 'home#index'
+  resources :users, only: [:create, :show]
+  # resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
 end
