4bf1c84aff8250411301f8e08a1a057fd196a64f
bouaik <bouaik.lhou@outlook.fr>
2020-08-06 22:18:58 +0100

    add validation to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,13 @@
 class Event < ApplicationRecord
+
+    validates :name, presence: true, length: { minimum: 6 }
+    validates :description, presence: true, length: { minimum: 30 }
+    validates :date, presence: true
+
+
+
+
     belongs_to :creator, class_name: :User
-    
     has_many :event_attendances, foreign_key: :attendances_id
     has_many :guests, through: :event_attendances, source: :attende
     
