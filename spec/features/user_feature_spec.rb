848abbab2b7a100ac754a0d009bdfa4971831e1c
Lhoussaine <bouaik.lhou@gmail.com>
2020-08-07 02:52:43 +0100

    make test for user log in

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -7,7 +7,7 @@ feature "capybara features", type: :feature do
         end
 
         scenario "with invalid inputs" do
-            fill_in "Name", with: "lhoussaine"
+            fill_in "user_name", with: "lhoussaine"
             click_on "Sign up"
             expect(page).to have_content("Email can't be blank")
         end
@@ -20,6 +20,27 @@ feature "capybara features", type: :feature do
             click_on "Sign up"
             expect(page).to have_content("You signed up successfully.")
         end
+    end
 
+    feature "log in a user" do
+        before(:each) do
+            visit "/login"
+        end
+
+        scenario "with invalid inputs" do
+            fill_in "name", with: "lhoussaine"
+            click_on "Log in"
+            expect(page).to have_content("Email or password is invalid")
+        end
+
+        scenario "with valid inputs" do
+            user = FactoryBot.create(:user)
+            within('form') do
+                fill_in "name", with: user.name
+                fill_in "email", with: user.email
+            end
+            click_on "Log in"
+            expect(page).to have_content("Yoy logged in successfully.")
+        end
     end
 end
