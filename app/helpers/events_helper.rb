7b3b6b3d7b9989c6e78df7886d890b0cb7fd6755
Lhoussaine <bouaik.lhou@gmail.com>
2020-08-11 03:45:39 +0100

    REFACTOR USER ERRORS TO HELPER

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,5 @@
 module EventsHelper
   def event_errors(event)
-    render 'events/errors', event: event if event.errors.any?
+    render 'events/errors', event: event, model: 'event' if event.errors.any?
   end
 end
